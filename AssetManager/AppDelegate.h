b4c307abfc87139b2ad265b34d6d03e2ceb3cebc
Rick Phan <hphu90@gmail.com>
2014-02-11 06:47:42 -0800

    Initial Commit

---
+++ AssetManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AssetManager
+//
+//  Created by HP on 2/11/14.
+//  Copyright (c) 2014 HP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
